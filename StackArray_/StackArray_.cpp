0e166fb7eec0442637f99e1a67dda86824a6643f
afnan030503 <muhammadafnanmaulidi@gmail.com>
2023-06-16 18:24:27 +0700

    langkah ke 3 decrement

--- StackArray_/StackArray_.cpp
+++ StackArray_/StackArray_.cpp
@@ -38,7 +38,7 @@ public:
 		}
 
 		cout << "\nThe popped element is: " << stack_array[top] << endl;	//step 2
-		top--;	//step 3 decrement
+		top--;//step 3 decrement
 	}
 
 	//method for check if data is empty
